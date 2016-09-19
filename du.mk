dc63d3b342ec97c37748bb3a9b9b31fd60dd4db7
yairpatch <yair.zarfati@gmail.com>
2016-09-19 15:37:31 +0300

    du device tree

---
+++ du.mk
@@ -0,0 +1,18 @@
+$(call inherit-product, device/lge/d855/device.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/nfc_enhanced.mk)
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_DEVICE := d855
+PRODUCT_NAME := du_d855
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-D855
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_global_com" \
+    BUILD_FINGERPRINT="lge/g3_global_com/g3:6.0/MRA58K/15351124649f4:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_global_com-user 6.0 MRA58K 15351124649f4 release-keys"
